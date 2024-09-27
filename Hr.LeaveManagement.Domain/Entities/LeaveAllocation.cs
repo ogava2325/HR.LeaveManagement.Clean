dc66f2a9c0645f2a637affa40877a62533035bd0
ogava_2325 <dimapajuk2325@gmail.com>
2024-09-27 22:55:40 +0300

    Added EmployeeId to LeaveAllocation Entity

--- Hr.LeaveManagement.Domain/Entities/LeaveAllocation.cs
+++ Hr.LeaveManagement.Domain/Entities/LeaveAllocation.cs
@@ -8,4 +8,5 @@ public class LeaveAllocation : BaseEntity
     public int LeaveTypeId { get; set; }
 
     public int Period { get; set; }
+    public string EmployeeId { get; set; } = String.Empty;
 }
